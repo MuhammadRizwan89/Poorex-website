20a8777e299a90704f77f5c995b4efe7f373678f
MuhammadRizwan89 <mrizwansaeed89xclusive@gmail.com>
2024-05-09 13:14:21 +0500

    two pages complete  donation 01 and 02

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,11 @@
 // hamber button
+humburger = document.querySelector('.humburger');
+closebtn = document.querySelector('.closebtn');
+
+humburger.addEventListener('click', function(){
+    document.querySelector('.tpoffcanvas-area').classList.add('active');
+})
+
+closebtn.addEventListener('click', function(){
+    document.querySelector('.tpoffcanvas-area').classList.remove('active');
+})

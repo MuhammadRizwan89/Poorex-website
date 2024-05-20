c53c4d7103a2e13385cc220dfa6a4fcd974c2988
MuhammadRizwan89 <mrizwansaeed89xclusive@gmail.com>
2024-05-20 10:21:43 +0500

    poorex website all pages don Alhamdulilha

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,26 @@
+
+const swiper = new swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
 // hamber button
 humburger = document.querySelector('.humburger');
 closebtn = document.querySelector('.closebtn');
@@ -19,3 +42,5 @@ function increment(){
 function decrement(){
     document.querySelector(".minus-btn").innerHTML = count;
 }
+
+

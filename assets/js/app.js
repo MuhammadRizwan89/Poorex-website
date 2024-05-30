31dc7e9d4a98a52e63472d4edbd9b48be85a2427
MuhammadRizwan89 <mrizwansaeed89xclusive@gmail.com>
2024-05-30 10:29:23 +0500

    2nd header page nave bar menu section update and other pages different changing also.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,44 @@
-
-// hamber button
-humburger = document.querySelector('.humburger');
-closebtn = document.querySelector('.closebtn');
-console.log(humburger);
-console.log(closebtn);
-humburger.addEventListener('click', function(){
-    document.querySelector('.tpoffcanvas-area').classList.add('active');
-})
-
-closebtn.addEventListener('click', function(){
-    document.querySelector('.tpoffcanvas-area').classList.remove('active');
-})
-
 //Quantity page shop details
-let count = 0;
 
-function increment(){
-    document.querySelector(".plus-btn").innerHTML = count;
-}
-function decrement(){
-    document.querySelector(".minus-btn").innerHTML = count;
-}
+// let count = document.querySelector("#count")
+// let output = 1;
+
+// function increment(){
+//     document.querySelector(".plus-btn").innerHTML = output += 2;
+//     output.innerHTML = count
+// }
+// function decrement(){
+//     document.querySelector(".minus-btn").innerHTML = output -= 2;
+//     output.innerHTML = count
+// }
+
+// ......................
+var a = document.querySelector(".plus-btn");
+var b = document.querySelector(".minus-btn");
+var c = document.querySelector("#count");
+var count = 1;
+
+a.addEventListener("click", () => {
+  count += 2;
+  c.innerHTML = count;
+});
+
+// for decrement
+
+b.addEventListener("click", () => {
+  if (count > 1) {
+    count -= 2;
+    c.innerHTML = count;
+  }
+});
+// let plus = document.querySelector(".plus-btn")
+// let minus = document.querySelector(".minus-btn")
+// let outPut = document.querySelector("#count")
 
 //hamburger navbar2
 
-aboutHamburger = document.querySelector('.about_hamburger');
+aboutHamburger = document.querySelector(".about_hamburger");
 
-aboutHamburger.addEventListener('click', function(){
-    document.querySelector('.navbar-nav').classList.toggle('menu');
-
-})
+aboutHamburger.addEventListener("click", function () {
+  document.querySelector(".navbar-nav").classList.toggle("menu");
+});
